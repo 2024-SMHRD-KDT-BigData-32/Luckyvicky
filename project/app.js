d640e171e09c00732007fe4f0474fc0f3f32a187
henwooman <gcoin09@gmail.com>
2025-04-02 10:26:14 +0900

    송현우 10:25
    app.js 수정, public 폴더 추가

--- project/app.js
+++ project/app.js
@@ -1,6 +1,33 @@
-// 라우터 등록
+const express = require("express")
+const app = express();
+const path = require("path")
+const bp = require("body-parser");
+const mainRouter = require("./routes/mainRouter");
+const userRouter = require("./routes/userRouter");
+const session = require("express-session");
+const fileStore = require('session-file-store')(session);
+
+// css 출력
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// post 데이터 처리 등록
+app.use(bp.urlencoded({extended : true}));
 
 // 세션 설정
+app.use(session({
+    httpOnly : true,
+    resave : false,
+    secret : "secret",
+    store : new fileStore(),
+    saveUninitialized : false
+}))
+
+
+
+// 라우터 등록
+app.use("/", mainRouter);
+app.use("/user", userRouter);
 
 
 
