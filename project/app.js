75019dbf69ebabea60bb7482435957b9894ec098
henwooman <gcoin09@gmail.com>
2025-04-03 12:16:12 +0900

    송현우 12:15
    메인페이지 틀, 현재위치 기반 주유소 지도, 로그인회원가입 기능 추가

--- project/app.js
+++ project/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express();
+const nunjucks = require("nunjucks");
 const path = require("path")
 const bp = require("body-parser");
 const mainRouter = require("./routes/mainRouter");
@@ -14,6 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // post 데이터 처리 등록
 app.use(bp.urlencoded({extended : true}));
 
+
 // 세션 설정
 app.use(session({
     httpOnly : true,
@@ -24,13 +26,17 @@ app.use(session({
 }))
 
 
-
 // 라우터 등록
 app.use("/", mainRouter);
 app.use("/user", userRouter);
 
 
-
+// 넌적스 셋팅
+app.set("view engine", "html")
+nunjucks.configure("views", {
+    express : app,
+    watch : true
+});
 
 
 app.listen(3000);
